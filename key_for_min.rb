48cf7f3d4e508b18f5fb2047be32b28ba42f23c1
taroj khan <tarujkhan@yahoo.com>
2020-02-02 19:56:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-changed_array : name_hash.size do |name_hash|
-  if name_hash < changed_array
+changed_array: name_hash.size do |name_hash|
+  if name_hash.size < changed_array
     puts NIL
   else
     puts name_hash
