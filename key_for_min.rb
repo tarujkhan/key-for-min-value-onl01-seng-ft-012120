d8d6f42a4448a033eacd31e19865083e6b882197
taroj khan <tarujkhan@yahoo.com>
2020-02-02 20:22:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,15 +9,14 @@ def key_for_min_value(name_hash)
    # puts changed_array
   #end
 #end  
-
-
-smallest_value = 0
- empty_hash = nil
- name_hash.each do |name, value|
-  if smallest_value == 0 || value < smallest_value
-    smallest_value = value
-    empty_hash = name
-  end
- end
- empty_hash
 end
+
+#smallest_value = 0
+ #empty_hash = nil
+ #if smallest_value == 0 || value < smallest_value
+  #  smallest_value = value
+   # empty_hash = name
+  #end
+ #end
+ #empty_hash
+#end
